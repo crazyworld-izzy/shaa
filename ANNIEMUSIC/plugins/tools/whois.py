5bed4dfd75e0ac4697421dbaa0a95ff1409cf78d
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-26 11:29:37 +0530

    Update whois.py

--- ANNIEMUSIC/plugins/tools/whois.py
+++ ANNIEMUSIC/plugins/tools/whois.py
@@ -21,11 +21,11 @@ def ReplyCheck(message: Message):
 
 infotext = (
     "[{full_name}](tg://user?id={user_id})\n\n"
-    " ➻ ᴜsᴇʀ ɪᴅ: `{user_id}`\n"
-    " ➻ ғɪʀsᴛ ɴᴀᴍᴇ: `{first_name}`\n"
-    " ➻ ʟᴀsᴛ ɴᴀᴍᴇ: `{last_name}`\n"
-    " ➻ ᴜsᴇʀɴᴀᴍᴇ: `@{username}`\n"
-    " ➻ ʟᴀsᴛ sᴇᴇɴ: `{last_online}`"
+    " ღ ᴜsᴇʀ ɪᴅ: `{user_id}`\n"
+    " ღ ғɪʀsᴛ ɴᴀᴍᴇ: `{first_name}`\n"
+    " ღ ʟᴀsᴛ ɴᴀᴍᴇ: `{last_name}`\n"
+    " ღ ᴜsᴇʀɴᴀᴍᴇ: `@{username}`\n"
+    " ღ ʟᴀsᴛ sᴇᴇɴ: `{last_online}`"
 )
 
 
@@ -52,7 +52,7 @@ def FullName(user: User):
     return user.first_name + " " + user.last_name if user.last_name else user.first_name
 
 
-@app.on_message(filters.command("whois"))
+@app.on_message(filters.command("info"))
 async def whois(client, message):
     cmd = message.command
     if not message.reply_to_message and len(cmd) == 1:
