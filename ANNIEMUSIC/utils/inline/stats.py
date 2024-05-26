2cfb5caf01a17a93230d0e8828d78fd110725828
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-26 18:30:50 +0530

    Update stats.py

--- ANNIEMUSIC/utils/inline/stats.py
+++ ANNIEMUSIC/utils/inline/stats.py
@@ -5,7 +5,7 @@ def stats_buttons(_, status):
     not_sudo = [
         InlineKeyboardButton(
             text=_["SA_B_1"],
-            callback_data="TopOverall",
+            callback_data="ღ TopOverall ღ",
         )
     ]
     sudo = [
@@ -15,7 +15,7 @@ def stats_buttons(_, status):
         ),
         InlineKeyboardButton(
             text=_["SA_B_3"],
-            callback_data="TopOverall",
+            callback_data="ღ TopOverall ღ",
         ),
     ]
     upl = InlineKeyboardMarkup(
@@ -24,7 +24,7 @@ def stats_buttons(_, status):
             [
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
-                    callback_data="close",
+                    callback_data="ღ close ღ",
                 ),
             ],
         ]
@@ -42,7 +42,7 @@ def back_stats_buttons(_):
                 ),
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
-                    callback_data="close",
+                    callback_data="ღ close ღ",
                 ),
             ],
         ]
